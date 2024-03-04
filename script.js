afc080809eadaee8254e40cd6500c50933919bad
Denys <denysbraun1985@gmail.com>
2024-03-04 15:16:41 +0200

    fix link to font and carousel options

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ $(document).ready(function(){
         speed: 1000,
         slidesToShow: 1,
         autoplay: true,
-        // autoplaySpeed: 5000,
-        // pauseOnHover: false,
+        autoplaySpeed: 5000,
+        pauseOnHover: false,
         pauseOnFocus: false
     });
 })
