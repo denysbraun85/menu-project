57c4e7897c65e0aa7b330ffb2931657fb9a3c0cd
Denys <denysbraun1985@gmail.com>
2024-03-07 16:17:38 +0200

    change carousel speed

--- script.js
+++ script.js
@@ -405,7 +405,7 @@ function runCarousel() {
         speed: 1000,
         slidesToShow: 1,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 45000,
         pauseOnHover: false,
         pauseOnFocus: false
     });
